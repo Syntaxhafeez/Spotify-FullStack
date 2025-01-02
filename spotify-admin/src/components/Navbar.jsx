3ce4e2519e7f8dd07b9cf695f4e73370502fd3ec
Syntaxhafeez <syntaxhafeez@gmail.com>
2025-01-02 19:44:24 +0530

    `Update Navbar component with new styles and copyright information`

--- spotify-admin/src/components/Navbar.jsx
+++ spotify-admin/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='navbar w-full border-b-2 border-gray-800 px-5 sm:px-12 py-4 text-lg'>
-      <p>Admin Panel</p>
+    <div className='navbar w-full border-b-2 border-gray-800 px-4 sm:px-12 py-3 text-lg'>
+      <p className="text-sm text-gray-400">Admin Panel  -  © 2025 Mohammad Hafeez. All rights reserved.</p>
     </div>
   )
 }
